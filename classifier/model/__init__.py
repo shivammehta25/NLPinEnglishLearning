f1a04c50d5ddd5400131ba5a30ca0c63d14ef22f
Shivam Mehta <shivam.mehta007@gmail.com>
2020-04-07 17:08:37 +0200

    Changing directory structure of classifiers

--- classifier/model/__init__.py
+++ classifier/model/__init__.py
@@ -2,6 +2,5 @@
 Diretory for model related information
 """
 
-from .RNNHiddenClassifier import RNNHiddenClassifier
-from .RNNMaxpoolClassifier import RNNMaxpoolClassifier
-from .CNNClassifier import CNN2dClassifier, CNN1dClassifier
+from .RNNClassifiers import RNNHiddenClassifier, RNNMaxpoolClassifier
+from .CNNClassifiers import CNN2dClassifier, CNN1dClassifier
